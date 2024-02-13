9287ca2ea9ecfc7155aca5e9b808a3cbcc32ad0a
vivek-panchal <vivek.nsut@gmail.com>
2024-02-13 13:15:39 +0530

    Project setup & Routes

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom"
+import { Home, Login, Profile, Register, ResetPassword } from "./pages";
+
+function Layout() {
+  const user = null;
+  const location = useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/login' state={{ from: location }} replace />
+  );
+}
+
+function App() {
+
+  
+
+  return (
+    <div className='w-full min-h-[100vh]'>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile/:id?' element={<Profile />} />
+        </Route>
+
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/ResetPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResetPassword() {
+  return (
+    <div>
+      Reset password page
+    </div>
+  )
+}
+
+export default ResetPassword
